11ae0b44ff58c0755507bf93d22bda270c4cb1bb
Matsukura <heuchannsight@yahoo.co.jp>
2020-07-04 11:00:16 +0900

    [新規作成]todoを一覧表示、詳細表示できるようにした。
    基本的なtodo機能を追加したかったため。

--- todo/gui_object.py
+++ todo/gui_object.py
@@ -7,8 +7,8 @@ class Frame(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
         self.grid(column=0, row=0)
-        self["width"] = 600
-        self["height"] = 600
+        self["width"] = 100
+        self["height"] = 100
         self["padx"] = 20
         self["pady"] = 20
 
@@ -21,5 +21,47 @@ class Label(tk.Label):
         self["width"] = 50
         self["anchor"] = "e"
         self["padx"] = 10
-        self["pady"] = 10
+        self["pady"] = 20
+        self["highlightcolor"] = "blue"
 
+
+class Text(tk.Text):
+    def __init__(self, master=None):
+        tk.Text.__init__(self, master)
+        self["width"] = 100
+        self["height"] = 10
+
+
+class Listbox(tk.Listbox):
+    def __init__(self, master=None, master_of_detail_text=None):
+        scrollbar = Scrollbar(master)
+        scrollbar.pack(side=RIGHT, fill=Y)
+        tk.Listbox.__init__(self, master, yscrollcommand=scrollbar.set, selectmode=EXTENDED)
+
+        self.pack(side=LEFT, fill=BOTH)
+        self["width"] = 100
+        self["height"] = 10
+        self["font"] = ("Helvetica", 12)
+        self.master = master
+        self.master_of_detail_text = master_of_detail_text
+        self.text = Text(self.master_of_detail_text)
+
+        scrollbar["command"] = self.yview
+        self.bind("<Double-Button-1>", self.show_detail)
+        self.bind("<Return>", self.show_detail)
+
+        self.todo_list = {}
+
+    def show_detail(self, event=None):
+        self.text.destroy()
+        self.text = Text(self.master_of_detail_text)
+        self.text.insert(END, self.read_detail_of_todo(self.index(ACTIVE)))
+        self.text.pack()
+
+    def set_todo_list(self, todo_list_dict):
+        self.todo_list = todo_list_dict
+
+    def read_detail_of_todo(self, index):
+        path = self.todo_list[index]
+        with open(path, encoding="utf_8") as f:
+            return f.read()
--- todo/todo.py
+++ todo/todo.py
@@ -19,23 +19,3 @@ class Todo:
         for index, dir_name in enumerate(self.dir_names):
             paths[self.dir_name_keys[index]] = list(all_files(dir_name, ";".join(self.patterns)))
         return paths
-
-"""    
-            
-        for path in paths:
-
-            #ファイル名記入
-            line_list.append("File Name : ")
-            line_list.append(path)
-            line_list.append("\n")
-
-            for line in range(line_number):
-                line_list.append(linecache.getline(path, line + 1))
-            line_list.append("\n")
-            line_list.append("================================================")
-            line_list.append("\n")
-
-        file_object = open("list.txt", "w")
-        file_object.writelines(line_list)
-        file_object.close()
-"""
